7dcbdbae9ae0c220673ee1717428548d7a3d8b83
Brandon Jones <cerealfordinnerx@gmail.com>
2021-12-07 21:37:12 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+const { Client, Intents } = require("discord.js");
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+const mySecret = process.env["TOKEN"];
+
+function getQuote() {
+  return fetch("https://zenquotes.io/api/random")
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      return data[0]["q"] + " - " + data[0]["a"];
+    });
+}
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("message", (msg) => {
+  if (msg.author.bot) return;
+
+  if (msg.content === "$inspire") {
+    getQuote().then((quote) => msg.channel.send(quote));
+  }
+});
+
+client.login("OTE3OTU5ODI3MjU3NDI1OTQx.YbAStQ.SYTYpwn0WoHG26kOLze_yG6pLoo");
